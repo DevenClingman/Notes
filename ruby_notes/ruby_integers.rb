a87578ba1a62142b1ca332e7ca9024ece94e940b
Deven Clingman <dclingy@gmail.com>
2017-07-17 16:44:43 -0600

    added more notes regarding html and ruby

---
+++ ruby_notes/ruby_integers.rb
@@ -0,0 +1,17 @@
+  # exponets        == **
+  # addition        == +
+  # subtraction     == -
+  # division        == /
+  # multiplication  == *
+
+### Order of Operations
+## Please Excuse My Dear Aunt Sally
+
+  # P - Paratheses
+  # E - Exponets
+  # M - Multiplication
+  # D - Division
+  # A - Addition
+  # S - Subration
+
+
